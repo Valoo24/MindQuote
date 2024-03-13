9861577b1c2224840640766a5a2669e63510c36c
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2024-03-13 11:37:50 +0100

    Added a quoteDTO

---
+++ MindQuote.Core/DTOs/QuoteDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MindQuote.Core.DTOs;
+
+public class QuoteDTO
+{
+    public string Content { get; set; }
+    public string AuthorFirstName { get; set; }
+    public string AuthorLastName { get; set; }
+    public string Origin { get; set; }
+}
